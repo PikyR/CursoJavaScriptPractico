af102cb174aa81edfbfc3f4dc2264676f7a91446
pikyr <pikytdv@gmail.com>
2022-01-04 23:51:47 -0300

    Fin de clase 13 - Funciones más inteligentes

--- precios-y-descuentos/js/scripts.js
+++ precios-y-descuentos/js/scripts.js
@@ -1,3 +1,22 @@
+const cupons = [
+  {
+    name: "cupon10",
+    discount: 10
+  },
+  {
+    name: "cupon30",
+    discount: 30
+  },
+  {
+    name: "cupon50",
+    discount: 50
+  },
+  {
+    name: "cupon100",
+    discount: 100
+  }
+];
+
 function calcularPrecioConDescuento(precio, descuento) {
   const porcentajeParaDescuento = 100 - descuento;
   const precioConDescuento = (precio * porcentajeParaDescuento) / 100; 
@@ -5,3 +24,27 @@ function calcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
+function onClickCalculateFinalPrice() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = Number(inputPrice.value);
+  
+  const inputCupon = document.getElementById("InputCupon");
+  const cuponValue = inputCupon.value;
+
+  const isCuponValueValid = function(cupon) {    
+    return cupon.name === cuponValue;
+  };
+ 
+  const userCupon = cupons.find(isCuponValueValid);
+
+  if (!userCupon) {
+    alert("Error")
+  } else {
+    const discount = userCupon.discount;
+    
+    const finalPrice = calcularPrecioConDescuento(priceValue, discount);
+
+    const finalPriceP = document.getElementById("FinalPriceP");
+    finalPriceP.innerText = "Priecio final $" + finalPrice;
+  }  
+}
