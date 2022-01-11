1e7332c8bf2cafd8ab2681a638244d01234005af
pikyr <pikytdv@gmail.com>
2022-01-11 00:13:29 -0300

    Fin de clase 16 - Calculando la media

--- promedioModaMedia/js/media.js
+++ promedioModaMedia/js/media.js
@@ -1,1 +1,59 @@
-alert("a")
+const listaMedia = [
+  100,
+  11,
+  200,
+  44,
+  350,
+  444,
+  8554,
+  1000000
+];
+
+function calcularMediaAritmetica(lista) {
+  const sumaLista = lista.reduce(
+    function(valorAcumulado = 0, nuevoElemento) {
+      return valorAcumulado + nuevoElemento;
+    }
+  );
+
+  const promedioLista = sumaLista / lista.length;
+  
+  return promedioLista;
+}
+
+function esPar(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function calcularMedia(lista) {
+  lista.sort(
+    function (a, b) {
+      return a - b;
+    }
+  );
+
+  const mitadLista = parseInt(lista.length / 2);
+  
+  let media;
+
+  if (esPar(lista.length)) {
+    const elemento1 = lista[mitadLista - 1];
+    const elemento2 = lista[mitadLista];
+  
+    const promedio = calcularMediaAritmetica([
+      elemento1,
+      elemento2
+    ]);
+  
+    media = promedio;
+  
+  } else {
+    media = lista[mitadLista];
+  }
+
+  return "Media: " + media;
+}
